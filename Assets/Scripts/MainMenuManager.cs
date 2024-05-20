2cef3a82d97f5b32697560678fe671953a8fbe8c
Sain <turtle27799@gmail.com>
2024-05-20 15:32:28 +0200

    MainMenuComplete

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,14 +4,46 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject credits;
+    [SerializeField] private GameObject controls;
+
+    public void StartAdventure()
+    {
+        mainMenu.SetActive(false);
+        credits.SetActive(false);
+        controls.SetActive(true);
+    }
+
     public void StartGame()
     {
-        print("Start Game");
         UnityEngine.SceneManagement.SceneManager.LoadScene("Movement");
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Credits()
+    {
+        mainMenu.SetActive(false);
+        credits.SetActive(true);
+        controls.SetActive(false);
+    }
+
+    public void BackToMain()
+    {
+        mainMenu.SetActive(true);
+        credits.SetActive(false);
+        controls.SetActive(false);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && (credits.activeSelf || controls.activeSelf))
+        {
+            BackToMain();
+        }
     }
 }
