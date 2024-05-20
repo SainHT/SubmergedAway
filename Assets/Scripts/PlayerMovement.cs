8452e373fb1184a88c3ab485a3cccdd30bc8e187
Simon Borovsky <simon.borovsky@gmail.com>
2024-05-20 12:03:11 +0200

    movement animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector2 direction;
+    [SerializeField] private Animator animator;
     [SerializeField] private float moveSpeed = 5f;
 
     void Start()
@@ -19,8 +20,22 @@ public class PlayerMovement : MonoBehaviour
         float dirY = Input.GetAxisRaw("Vertical");
 
         direction = new Vector2(dirX, dirY);
+
+        animator.SetFloat("Vertical", dirY);
+        animator.SetFloat("Speed", direction.sqrMagnitude);
+        
         direction.Normalize();
 
+
         rb.velocity = direction * moveSpeed;
+
+        if (dirX == -1f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (dirX == 1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 }
