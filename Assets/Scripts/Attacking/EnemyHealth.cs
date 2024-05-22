474e568157d1fc7d85b1e107fcfe40bc9df77d4b
Simon Borovsky <simon.borovsky@gmail.com>
2024-05-22 15:58:29 +0200

    fish death animation

--- Assets/Scripts/Attacking/EnemyHealth.cs
+++ Assets/Scripts/Attacking/EnemyHealth.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Rigidbody2D rb;
     [SerializeField] private int health;
     [SerializeField] private int maxHealth = 100;
     [SerializeField] private int healOnDeath = 1;
@@ -13,6 +14,7 @@ public class EnemyHealth : MonoBehaviour
     private FloatingStatusBar floatingHealthBar;
     private PlayerHealth playerHealth;
     private float hurtCooldown = 0.09f;
+    private float deathTime = 0.05f;
 
     private void Awake()
     {
@@ -35,7 +37,7 @@ public class EnemyHealth : MonoBehaviour
 
         if (health <= 0)
         {
-            Die();
+        StartCoroutine(Die());
         }
 
         StartCoroutine(HurtCooldown());
@@ -46,9 +48,13 @@ public class EnemyHealth : MonoBehaviour
         yield return new WaitForSeconds(hurtCooldown);
         animator.SetFloat("Hurt", 0);
     } 
-
-    void Die()
+    
+    private IEnumerator Die()
     {
+        rb.constraints = RigidbodyConstraints2D.FreezeAll;
+        animator.SetFloat("Death", 1);
+        yield return new WaitForSeconds(1f);
+        animator.SetFloat("Death", 0);
         playerHealth.Heal(healOnDeath);
         Destroy(gameObject);
     }
